368c45f1673cf5992690d81d5dfaa816e3ed497c
Alexander Logger <intagger@live.ru>
2013-12-13 01:13:22 +0400

    Added SharpMTProto.PCL project with base classes for MTProto. Added SharpTL as submodule.

---
+++ src/SharpMTProto/SharpMTProto.PCL/MTProtoClient.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MTProtoClient.cs">
+//   Copyright (c) 2013 Alexander Logger. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SharpMTProto
+{
+    public class MTProtoClient : ITLMethods
+    {
+        public ResPQ req_pq(req_pq args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Server_DH_Params req_DH_params(req_DH_params args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Set_client_DH_params_answer set_client_DH_params(set_client_DH_params args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RpcDropAnswer rpc_drop_answer(rpc_drop_answer args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public FutureSalts get_future_salts(get_future_salts args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Pong ping(ping args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Pong ping_delay_disconnect(ping_delay_disconnect args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DestroySessionRes destroy_session(destroy_session args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

38204d4a39d92e5e3750dd6b147e2f19355072e2
Alexander Logger <intagger@live.ru>
2014-01-13 01:49:24 +0400

    Updated version to 0.1.1.

--- src/SharpMTProto/CommonAssemblyInfo.cs
+++ src/SharpMTProto/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]

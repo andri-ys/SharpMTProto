19a422440a3dde9fd0a586bfc56a892d1b2f7660
Alexander Logger <intagger@live.ru>
2014-01-14 18:04:32 +0400

    Fixed build scripts errors.

--- build/Build-Debug.ps1
+++ build/Build-Debug.ps1
@@ -2,4 +2,4 @@ Import-Module psake;
 Import-Module pscx;
 $build_dir = Split-Path $script:MyInvocation.MyCommand.Path;
 Invoke-psake $build_dir\Default.ps1 -properties @{'config'='Debug'};
-'Release building completed' | Out-Speech;
+'Debug building completed' | Out-Speech;

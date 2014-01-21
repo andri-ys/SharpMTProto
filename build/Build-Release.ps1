6e686b4e3cbc6c9dfe43c5737102ce8d95260673
Alexander Logger <intagger@live.ru>
2014-01-22 01:12:05 +0400

    Updated build scripts.

--- build/Build-Release.ps1
+++ build/Build-Release.ps1
@@ -1,5 +1,2 @@
 Import-Module psake;
-Import-Module pscx;
-$build_dir = Split-Path $script:MyInvocation.MyCommand.Path;
-Invoke-psake $build_dir\Default.ps1 -properties @{'config'='Release'};
-'Release building completed' | Out-Speech;
+Invoke-psake .\Default.ps1 -properties @{'config'='Release'};
